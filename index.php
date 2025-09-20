a02ae1207b5e5a02031cb09083755be8ccba53f6
Esteban-Developer <estebancontacto.dev@gmail.com>
2025-09-19 19:38:44 -0500

    Primer commit del proyecto jenkins-php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Mostrar información básica
+echo "<h1>¡Hola, Jenkins con PHP!</h1>";
+echo "<p>Este es un index.php sencillo corriendo en Docker 🚀</p>";
+
+// Mostrar fecha y hora actual
+echo "<p>Fecha y hora actual: " . date("Y-m-d H:i:s") . "</p>";
+
+// Probar que PHP está funcionando
+phpinfo();
+?>
